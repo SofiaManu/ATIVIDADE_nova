fd62b6f0a4f4dc9cac86bb4c0c5a401932d3ea9f
SofiaManu <165834630+SofiaManu@users.noreply.github.com>
2024-12-05 17:34:35 +0000

    mudança

---
+++ bd.js
@@ -0,0 +1,28 @@
+//bd.js
+import pkg from "pg";
+const { Pool } = pkg;
+
+async function connect() {
+  const pool = new Pool({
+    connectionString: process.env.URL_BD,
+  });
+  return pool.connect();
+}
+
+async function selectUsuarios() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM usuario");
+	  client.release();
+  return res.rows;
+}
+
+export { selectUsuarios };
+async function selectUsuario(id) {
+    const client = await connect();
+    const query = "SELECT * FROM usuario WHERE id = $1";
+    const usuario = [id];
+    const res = await client.query(query, usuario);
+    client.release();
+    return res.rows;
+}
+export { selectUsuarios, selectUsuario };
--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-import express from "express";      // Requisição do pacote do express
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+//index.js
+import dotenv from "dotenv";
+import express from "express";
+import { selectUsuarios } from "./bd.js";
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
-  console.log("Rota GET/ solicitada");
+dotenv.config();
+
+const app = express(); // Instancia o Express
+const port = 3001; // Define a porta
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
   res.json({
-    nome: "Seu_nome_completo",      // Substitua pelo seu nome
+    nome: "Sofia Dias", // Substitua pelo seu nome
   });
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuarios solicitada");
+});
+
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
+});
+import { selectUsuarios, selectUsuario } from "./bd.js";
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario/# solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
